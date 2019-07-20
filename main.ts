adca7609a86791a4c680bc33120909840fa3c9d1
huanglei <code.huanglei@gmail.com>
2019-07-20 21:16:12 +0800

    modofy

--- main.ts
+++ main.ts
@@ -40,32 +40,71 @@ namespace ylwl_IoT {
     // 关闭
     const AT_CIPCLOSE: string = 'AT+CIPCLOSE'
 
-    //% weigth=99
-    //% blockId="ylwl_iot_wifi_init" block="connect to WiFi with tx %wifiTX rx %wifiRX baud %baudRate"
+    let connectHandle: () => void
+
+    let connectedWiFi: boolean = false
+
+    //% weight=99
+    //% blockId="ylwl_iot_wifi_init" block="init WiFi with tx %wifiTX rx %wifiRX baud %baudRate"
     //% wifiTX.defl=SerialPin.P16 wifiRX.defl=SerialPin.P8 baudRate=BaudRate.BaudRate115200
-    export function connectToWiFi(wifiTX: SerialPin = SerialPin.P16, wifiRX: SerialPin = SerialPin.P8, baudRate: BaudRate = BaudRate.BaudRate115200): void {
+    export function initWiFi(wifiTX: SerialPin = SerialPin.P16, wifiRX: SerialPin = SerialPin.P8, baudRate: BaudRate = BaudRate.BaudRate115200): void {
         serial.redirect(wifiTX, wifiRX, baudRate)
         basic.pause(500)
         writeToSerial(AT_RST)
         writeToSerial(AT_CWMODE_CUR)
         writeToSerial(AT_CWAUTOCONN)
+        basic.pause(1000)
+        connectedWiFi = waitResponse()
+        if (connectedWiFi) {
+            basic.showIcon(IconNames.Happy)
+        } else {
+            basic.showIcon(IconNames.Sad)
+        }
     }
 
-    //% weight=98
+    //% weight=89
     //% blockId="ylwl_iot_wifi_connect" block="connect to WiFi network ssid %ssid password %password"
     //% ssid.defl="ssid" password.defl="password"
-    export function connectToWiFiNetwork(ssid: string, password: string): void {
-        writeToSerial(`${AT_CWJAP_CUR}="${ssid}","${password}"`, 10000)
-        basic.showIcon(IconNames.Yes)
+    export function connectToWiFi(ssid: string, password: string): void {
+        // if (connectedWiFi) {
+            writeToSerial(`${AT_CWJAP_CUR}="${ssid}","${password}"`)
+            let connected = waitResponse()
+            if (connected) {
+                ok()
+                if (connectHandle) {
+                    connectHandle()
+                }
+            } else {
+                error()
+            }
+        // }
     }
 
-    //% weight=97
+    //% weight=88
     //% blockId="ylwl_iot_wifi_disconnect" block="disconnect from WiFi network"
-    export function diconnectWiFi(): void {
-        writeToSerial(AT_CWQAO)
+    export function diconnectFromWiFi(): void {
+        writeToSerial(AT_CIPCLOSE)
     }
 
-    //% weight=96
+    //% weight=79
+    //% blockId="ylwl_iot_on_wifi_connected" block="on WiFi connected"
+    export function onWiFiConnected(handle: () => void): void {
+        connectHandle = handle
+    }
+
+    //% weight=79
+    //% blockId="ylwl_iot_on_wifi_disconnected" block="on WiFi disconnected"
+    export function onWiFiDisconnected(handle: () => void): void {
+
+    }
+
+    //% weight=78
+    //% blockId="ylwl_iot_on_wifi_received" block="on WiFi received"
+    export function onWiFiReceived(handle: (data: string) => void): void {
+
+    }
+
+    //% weight=10
     //% blockId="ylwl_iot_request" block="execute HTTP method: %method|host: %host|port: %port|path: %path|headers: %headers|body: %body"
     //% method.defl=HttpMethod.GET port.defl=80
     export function request(method: HttpMethod, host: string, port: number, path: string, headers?: string, body?: string) {
@@ -94,18 +133,37 @@ namespace ylwl_IoT {
         writeToSerial(AT_CIPCLOSE)
     }
 
-    //% weight=95
+    //% weight=2
     //% blockId="ylwl_iot_execute_command" block="execute AT command %command and then wait %waitTime"
     //% command.defl="AT" waitTime.defl=200
     export function executeCommand(command: string, waitTime: number = 200): void {
         writeToSerial(command, waitTime)
     }
 
-
     //% weight=1
     //% blockId="ylwl_iot_new_line" block="CRLF"
     export function newLine(): string {
         return NEW_LINE
+    }
+
+    function waitResponse(): boolean {
+        let str: string = ''
+        let result: boolean = false
+        let time: number = input.runningTime()
+        while (true) {
+            let s = serial.readString()
+            str += s
+            // if (str.length > 200) str = str.substr(str.length - 200)
+            if (str.includes('OK') || str.includes('ALREADY CONNECTED')) {
+                result = true
+                break
+            }
+            if (input.runningTime() - time > 10000) {
+                break
+            }
+            basic.pause(100)
+        }
+        return result
     }
 
     function loading(): void {
@@ -122,11 +180,14 @@ namespace ylwl_IoT {
         basic.showIcon(IconNames.Yes)
     }
 
+    function error(): void {
+        basic.showIcon(IconNames.No)
+    }
+
     function writeToSerial(str: string, waitTime: number = 200): void {
         loading()
         let cmd: string = str + NEW_LINE
         serial.writeString(cmd)
         basic.pause(waitTime)
-        ok()
     }
 }
